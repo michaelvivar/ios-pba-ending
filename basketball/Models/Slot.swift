7dad3941d36a6d1335aff0a7fe128e2bdcbb44bc
Michael Vivar <michaelvivar@michaels-macbook-pro.local>
2018-09-29 08:03:54 +0800

    *

---
+++ basketball/Models/Slot.swift
@@ -0,0 +1,20 @@
+//
+//  SlotModel.swift
+//  basketball
+//
+//  Created by Michael Vivar on 25/09/2018.
+//  Copyright © 2018 Michael Vivar. All rights reserved.
+//
+
+import Foundation
+
+struct Slot: Codable {
+    let number: String
+    let name: String
+    let paid: Bool
+    let user: String
+}
+
+struct Slots: Codable {
+    let data: [Slot]
+}
